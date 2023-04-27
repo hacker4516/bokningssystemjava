3129aa808094ab68baeffef39c058ec2eaf332c9
hacker4516 <kenan.tas@elev.ga.ntig.se>
2023-04-27 15:26:37 +0200

    string

---
+++ src/bokning.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class bokning {
+
+    static Scanner fortnite = new Scanner(System.in);
+    public static void main (String [] args){
+        String fortnite1 = fortnite.nextLine();
+    }
+}
